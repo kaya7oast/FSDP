7fb9ba6f7bb368f9c837a85ebec49ffaf83230b4
Harangad Singh <harangad2007@gmail.com>
2025-11-01 12:23:01 +0800

    Moved the css code from the HTML file to a separate css file. Did the same thing for the js code.

---
+++ views/js/agentBuilder.js
@@ -0,0 +1,22 @@
+window.tailwind = window.tailwind || {};
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#137fec",
+        "background-light": "#f6f7f8",
+        "background-dark": "#101922",
+      },
+      fontFamily: {
+        "display": ["Inter", "sans-serif"]
+      },
+      borderRadius: {
+        "DEFAULT": "0.5rem",
+        "lg": "0.75rem",
+        "xl": "1rem",
+        "full": "9999px"
+      },
+    },
+  },
+};
