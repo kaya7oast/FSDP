5245b2f12a362d01b6654abe61097f4e3aef1ea6
weihanNP <s10228059@connect.np.edu.sg>
2025-11-05 23:12:56 +0800

    fix te file location, now the html can access the javascript.

--- app.js
+++ app.js
@@ -8,20 +8,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static assets from the public directory
+app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.get('/agent-builder', (req, res) => {
-	res.sendFile(path.join(__dirname, 'agentBuilder.html'));
+    res.sendFile(path.join(__dirname, 'views', 'agentBuilder.html'));
 });
 app.get('/homepage', (req, res) => {
-    res.sendFile(path.join(__dirname, 'agentHomepage.html'));
+    res.sendFile(path.join(__dirname, 'views', 'agentHomepage.html'));
 });
 app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, 'agentDashboard.html'));
+    res.sendFile(path.join(__dirname, 'views', 'agentDashboard.html'));
 });
 app.get('/agent-conversation', (req, res) => {
-    res.sendFile(path.join(__dirname, 'agentConversation.html'));
+    res.sendFile(path.join(__dirname, 'views', 'agentConversation.html'));
 });
 
 
