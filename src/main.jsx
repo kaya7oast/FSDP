bc845b1033e4a96c79ba2cf98547930160e27292
Harangad Singh <s10267942@connect.np.edu.sg>
2025-11-18 16:31:15 +0800

    Added agentHomepage react.js content, updated package.json, and updated main.jsx such that the server loads both pages.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './agentDashboard.jsx'
+import {BrowserRouter} from 'react-router-dom'
+import AgentDashboard from './agentDashboard.jsx'
+import AgentHomepage from './components/AgentHomepage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AgentDashboard />
+      <AgentHomepage />
+    </BrowserRouter>
   </StrictMode>,
 )
