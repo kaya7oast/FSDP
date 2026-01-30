42eff41778cc2f98f0bc761a71909f9b693e2441
mkaisheng <s10268302@connect.np.edu.sg>
2026-01-30 15:31:49 +0800

    fix voice assistant and conversations

--- src/components/VoiceAssistant.jsx
+++ src/components/VoiceAssistant.jsx
@@ -64,7 +64,7 @@ const VoiceAssistant = () => {
       fullStop();
       window.removeEventListener('ada:supervisor', handleSupervisorSignal);
     };
-  }, [navigate, token]);
+  }, [navigate]);
 
   // --- 2. CONTROLS ---
   const toggleSystem = () => {
