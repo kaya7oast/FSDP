c4df919a21ce1c59f2afab84454e9ace6fda9fad
Jayden-KKX <S10267761@connect.np.edu.sg>
2026-01-30 16:10:31 +0800

    analytics page changed to popularity

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,10 @@ import Layout from './components/Layout';
 import AgentDashboard from './components/agentDashboard';
 import AgentBuilder from './components/agentBuilder';
 import AgentConversation from './components/agentConversation.jsx/page';
-import AgentAnalytics from './components/agentAnalytics';
+import PopularityPage from './components/Popularity.jsx';
 import LoginPage from './components/login.jsx';
 import SignupPage from './components/signup.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -24,7 +25,7 @@ createRoot(document.getElementById('root')).render(
         {/* 2. Authenticated Routes: Wrapped in the Layout component */}
         <Route element={<Layout />}>
           <Route path="/dashboard" element={<AgentDashboard />} />
-          <Route path="/analytics" element={<AgentAnalytics />} />
+          <Route path="/popularity" element={<PopularityPage />} />
           <Route path="/chats" element={<AgentConversation />} />
           <Route path="/builder" element={<AgentBuilder />} />
           <Route path="/builder/:agentId" element={<AgentBuilder />} />
